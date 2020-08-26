e9c27c965363df77792872d474348c76a6ab1e51
Даниил <daniil1995.95@gmail.com>
2020-08-26 16:33:22 +0300

    DuplicateEncoder

---
+++ CodeWars/Kata/Middle/DuplicateEncoder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWars.Kata.Middle
+{
+    public class DuplicateEncoder
+    {
+        public static string DuplicateEncode(string word)
+        {
+            Dictionary<char, int> charsCnt = new Dictionary<char, int>();
+            foreach (var chr in word.ToLower())
+            {
+                if (charsCnt.TryGetValue(chr, out int val))
+                {
+                    charsCnt[chr]++;
+                }
+                else
+                {
+                    charsCnt.Add(chr, 1);
+                }
+            }
+            return string.Join(string.Empty, word.ToLower().Select(x => charsCnt[x] > 1 ? ')' : '('));
+        }
+    }
+}
