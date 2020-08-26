4933029a0da8a6b089944b181dd41c203d1b1783
Alexandr Peshkov <peshkov.alexandr.net@gmail.com>
2020-08-26 13:47:17 +0300

    Добавьте файлы проекта.

---
+++ CodeWars/Kata/Juniour/UniqueInOrderSolution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.Kata.Juniour
+{
+    public static class UniqueInOrderSolution
+    {
+        /// <summary>
+        /// Implement the function unique_in_order which takes
+        /// as argument a sequence and returns a list of items without any elements
+        /// with the same value next to each other and preserving the original order of elements.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="iterable"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            if (iterable == null)
+            {
+                yield return default;
+            }
+
+            IEnumerator<T> enumerator = iterable.GetEnumerator();
+            T curent = default;
+            //List<T> res = new List<T>(iterable.Count());
+
+            bool isMoved;
+            do
+            {
+                isMoved = enumerator.MoveNext();
+                if (isMoved)
+                {
+                    T prev = curent;
+                    curent = enumerator.Current;
+                    if (System.Object.Equals(prev, curent) == false)
+                    {
+                        yield return curent;
+                    }
+                }
+            } while (isMoved == true);
+        }
+    }
+}
---
+++ CodeWarsTests/Kata/Juniour/Task1Tests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars.Kata.Juniour;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CodeWarsTests.Kata.Juniour
+{
+    [TestFixture]
+    public class Task1Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.AreEqual("", UniqueInOrderSolution.UniqueInOrder(""));
+        }
+
+        [Test]
+        public void SimleTest1()
+        {
+            Assert.AreEqual("ABCDAB", UniqueInOrderSolution.UniqueInOrder("AAAABBBCCDAABBB"));
+        }
+
+        [Test]
+        public void NullTest1()
+        {
+            Assert.AreEqual(new List<string> { "A", "B", null, "" }, UniqueInOrderSolution.UniqueInOrder(new List<string> { "A", "B", "B", null, "" }));
+        }
+    }
+}
